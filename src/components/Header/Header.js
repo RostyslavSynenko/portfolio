67b4f361892ddc520c9e2d36c841ca49d7f127e4
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2020-05-24 23:37:49 +0300

    hiding/showing header during scrolling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Navigation from './Navigation';
 
 const Header = () => {
+  const [showHeader, setShowHeader] = useState(true);
+
+  useEffect(() => {
+    const toggleHeader = (() => {
+      let prevScrollTop = 0;
+
+      return () => {
+        const { scrollTop } = document.documentElement;
+        const scrollDiff = scrollTop - prevScrollTop;
+
+        if (
+          showHeader &&
+          scrollTop >= 100 &&
+          scrollDiff > 0
+        ) {
+          setShowHeader(false);
+        } else if (scrollDiff < 0) {
+          setShowHeader(true);
+        }
+
+        prevScrollTop = scrollTop;
+      };
+    })();
+
+    window.addEventListener('scroll', toggleHeader);
+
+    return () =>
+      window.removeEventListener('scroll', toggleHeader);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <header>
+    <header className={showHeader ? '' : 'hide-header'}>
       <div className="logo">
         <Link to="/">
           <i className="fab fa-connectdevelop"></i>Rostyslav
