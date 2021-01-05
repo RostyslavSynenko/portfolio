e5c7249e63800cb1ed0ea758b35fca313434e242
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2021-01-05 22:00:08 +0200

    [Client] Added Eslint. Fixed lint errors

--- client/src/components/HOC/withHttptService.js
+++ client/src/components/HOC/withHttptService.js
@@ -2,12 +2,28 @@ import React from 'react';
 
 import { HttpServiceConsumer } from '../HttpServiceContext';
 
-const withHttpService = () => Wrapped => props => (
-  <HttpServiceConsumer>
-    {httpService => (
-      <Wrapped {...props} httpService={httpService} />
-    )}
-  </HttpServiceConsumer>
-);
+const getDisplayName = WrappedComponent =>
+  WrappedComponent.displayName ||
+  WrappedComponent.name ||
+  'Component';
+
+const withHttpService = () => WrappedComponent => {
+  const WithHttpService = props => (
+    <HttpServiceConsumer>
+      {httpService => (
+        <WrappedComponent
+          {...props}
+          httpService={httpService}
+        />
+      )}
+    </HttpServiceConsumer>
+  );
+
+  WithHttpService.displayName = `WithHttpService(${getDisplayName(
+    WrappedComponent
+  )})`;
+
+  return WithHttpService;
+};
 
 export default withHttpService;
